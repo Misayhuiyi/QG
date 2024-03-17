e406a05a7b1826360d6251bce5e17c6b6cab7d8e
Misayhuiyi <163551382+Misayhuiyi@users.noreply.github.com>
2024-03-18 01:18:57 +0800

    优化

--- 大组/Week1/Singlelinkedlist/Headers/all.h
+++ 大组/Week1/Singlelinkedlist/Headers/all.h
@@ -5,13 +5,13 @@ typedef struct Linklist
 	int num;
 	struct Linklist* next;
 }Node;
-Node* head = NULL;
-void LinklistCreate();
-void LinklistIncrease(Node** head, int t, int n);
-void LinklistDelet(Node** head, int n);
-void LinklistDestroy(Node** head);
-void Linklistsearch(Node** head, int n);
-void print(Node** head);
-void LinklistReverse(Node** head);
-void  LinklistMiddle(Node** head);
+Node* head;
+Node* LinklistCreate();
+void LinklistIncrease( int t, int n);
+void LinklistDelet( int n);
+void LinklistDestroy();
+void Linklistsearch( int n);
+void print();
+void LinklistReverse( );
+void  LinklistMiddle( );
  
--- 大组/Week1/Singlelinkedlist/Sources/Link_list.cpp
+++ 大组/Week1/Singlelinkedlist/Sources/Link_list.cpp
@@ -1,6 +1,6 @@
 #include "all.h"
 
-void LinklistCreate()
+Node* LinklistCreate()
 {
 	head = (Node*)malloc(sizeof(Node));
 	if (head == NULL)
@@ -13,51 +13,60 @@ void LinklistCreate()
 	scanf_s("%d",&number);
 	head->next = NULL;
 	head->num = number;
+	return head;
 }
 
-void LinklistIncrease(Node**head ,int t,int n)
-{
-	int i = 1;
-	Node* p1 = (Node*)malloc(sizeof(Node));
-	Node* p = *head;
-	p1->num = t;
-	if (*head=NULL)
-	{
-		*head = p1;
-		(*head)->next = NULL;
-		return;
-	}
-	while (p->next!=NULL && i < n-1)
-	{
-		p = p->next;
-		i++;
-	}
-	if (p->next == NULL)
-	{
-		p->next = p1;
-		p1->next = NULL;
-		return;
-	}
-	if (i == 1)
-	{
-		p1->next = (*head)->next;
-		(*head)->next = p1;
-		return;
-	}
-	else
-	{
-		p1->next = p->next;
-		p->next = p1;
-		return;
-	}
-
-}
-
-void LinklistDelet(Node**head,int n)
+void LinklistIncrease(int t,int n)
 {	
-	Node* p1 = (*head);
+	
+	int i = 1;
+	Node* p = head;
+	Node* p1 = (Node*)malloc(sizeof(Node));
+	if (p1 != NULL) {
+
+		p1->num = t;
+
+		if (head == NULL)
+		{
+			head = p1;
+			(head)->next = NULL;
+			return;
+		}
+		while (p->next != NULL && i < n - 1)
+		{
+			p = p->next;
+			i++;
+		}
+		if (p->next == NULL)
+		{
+			p->next = p1;
+			p1->next = NULL;
+			return;
+		}
+		if (i == 1)
+		{
+			p1->next = (head)->next;
+			(head)->next = p1;
+			return;
+		}
+		else
+		{
+			p1->next = p->next;
+			p->next = p1;
+			return;
+		}
+	}
+
+}
+
+void LinklistDelet(int n)
+{	
+	if ((head) == NULL) {
+		return;
+	}
+	Node* p1 = (head);
 	Node*p2=NULL;
-	if ((*head) == NULL) {
+	if ((head) == NULL) {
 		printf("没有创建链表");
 		return;
 	}
@@ -72,19 +81,20 @@ void LinklistDelet(Node**head,int n)
 	}
 	if (p2 == NULL)//要删除第一个结点即头指针
 	{
-		*(head) = (*head)->next;
+		head = head->next;
 	}
 	else {
-		p2->next = p1->next;
+
+			p2->next = p1->next;
 	}
 	free(p1);
 }
 
-void LinklistDestroy(Node** head) {
-	if ((*head) == NULL) {
+void LinklistDestroy() {
+	if ((head) == NULL) {
 		return;
 	}
-	Node* p1 = (*head);
+	Node* p1 = (head);
 	Node* p2 = NULL;
 	while (p1 != NULL) {
 		p2 = p1;
@@ -92,16 +102,16 @@ void LinklistDestroy(Node** head) {
 		free(p2);//不断释放内存
 
 	}
-	(*head) = NULL;//销毁链表
+	(head) = NULL;//销毁链表
 }
 
 
-void Linklistsearch(Node** head,int n) {
+void Linklistsearch(int n) {
 	int i = 1;
-	if ((*head) == NULL) {
+	if ((head) == NULL) {
 		return;
 	}
-	Node* p = (*head);
+	Node* p = (head);
 	while (p->next != NULL && p->num != n)
 	{
 		p = p->next;
@@ -109,53 +119,65 @@ void Linklistsearch(Node** head,int n) {
 	}
 	if (p->num == n)
 	{
-		printf("该数据存在，编号为%d", i);
+		printf("该数据存在，编号为%d\n", i);
 		return;
 	}
 	if (p->next == NULL)
 	{
-		printf("没有找到该数据");
+		printf("没有找到该数据\n");
 	}
 		
 }
 
-void print(Node** head) {
-	Node* p = (*head);
+void print() {
+	if ((head) == NULL) {
+		return;
+	}
+	Node* p = (head);
 	while (p != NULL) {
 		printf("%d\n", p->num);
 		p = p->next;
 	}
 }
 
-void LinklistReverse(Node** head)
-{
+void LinklistReverse()
+{	
+	if ((head) == NULL) {
+		return;
+	}
 	Node* p;
 	Node* q;
-	p = (*head)->next;
+	p = (head)->next;
 	while (p->next)
 	{
 		q = p->next;//反转结点
 		p->next = q->next;//保存连接剩下的结点
-		q->next = (*head)->next;//q的下一结点指向新链表的第一个结点
-		(*head)->next = q;//将链表第一个结点改为q
+		q->next = (head)->next;//q的下一结点指向新链表的第一个结点
+		(head)->next = q;//将链表第一个结点改为q
 	}
 }
 
-void  LinklistMiddle(Node** head) {
-	Node* p1;
-	Node* p2;
-	p1 = (*head);
-	p2 = (*head);
-	while (p1->next) {
-		p1 = p1->next->next;
-		p2 = p1->next;
+void  LinklistMiddle() {
+	if ((head) == NULL) {
+		return;
 	}
-	printf("中间数据为%d", p2->num);
+	Node* p1=NULL;
+	Node* p2=NULL;
+	if (p1 != NULL && p2 != NULL) {
+		p1 = (head);
+		p2 = (head);
+		while (p1->next) {
+			p1 = p1->next->next;
+			p2 = p1->next;
+		}
+		printf("中间数据为%d\n", p2->num);
+	}
 }
 
 
 int main()
-{	
+{
+	head = NULL;
 	int num;
 	int t, n;
 	int command;
@@ -176,7 +198,7 @@ int main()
 
 		switch (command) {
 		case 1:
-			LinklistCreate();
+			head=LinklistCreate();
 			printf("创建链表成功\n");
 			break;
 		case 2:
@@ -185,34 +207,36 @@ int main()
 			scanf_s("%d", &t);
 			printf("请输入该数据的编号（头指针编号为1）：\n");
 			scanf_s("%d", &n);
-			LinklistIncrease(&head, t,n);
+			LinklistIncrease(t,n);
 			break;
 		case 3:
 			printf("请输入您要删除的数据\n");
 
 			scanf_s("%d", &num);
-			LinklistDelet(&head, num);
+			LinklistDelet( num);
+			printf("删除成功！\n");
 			break;
 		case 4:
-			LinklistMiddle(&head);
+			LinklistMiddle();
 			break;
 		case 5:
-			print(&head);
+			print();
 			break;
 		case 6:
 			printf("滴滴！执行销毁操作——\n");
-			LinklistDestroy(&head);
+			LinklistDestroy();
 			printf("销毁成功\n");
 			break;
 		case 7:
 			printf("请输入你要查找的数据:\n");
 			scanf_s("%d", &num);
-			Linklistsearch(&head, num);
+			Linklistsearch( num);
 			break;
 		case 8:
 			printf("滴滴！正在反转中——\n");
-			LinklistReverse(&head);
+			LinklistReverse();
 			printf("反转成功\n");
+			break;
 		default:
 			command = 9;
 			break;
